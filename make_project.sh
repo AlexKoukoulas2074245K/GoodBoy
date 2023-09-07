9d7ac3375c6d7903e36a3df1d738a24b3b9d8062
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2023-09-07 10:32:13 +0100

    Refactored CMake to not have any dependencies

--- make_project.sh
+++ make_project.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+echo "-- Checking for Brew installation"
+which -s brew
+if [[ $? != 0 ]] ; then
+  echo "-- Brew was not found. Installing via Ruby"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "-- Found Brew"   
+fi
+
+if brew ls --versions SDL2 > /dev/null; then
+  echo "-- Found SDL2 installation with Brew"
+else
+  echo "-- SDL2 was not found in the system. Installing via Brew"
+  brew install SDL2
+fi
+
+echo "-- Running CMAKE"
 mkdir project_files
 cd project_files
 cmake -G Xcode ../source/
+
